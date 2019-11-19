2411fbe93895b5f1f9271e1479b8d3741d52ac76
Qingnan Zhou <qzhou@adobe.com>
2019-11-18 21:08:15 -0800

    Add circle tests.

--- tests/test_NURBS.cpp
+++ tests/test_NURBS.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include <cmath>
+#include <iostream>
 #include <nanospline/NURBS.h>
 
 TEST_CASE("NURBS", "[nurbs]") {
@@ -35,5 +37,81 @@ TEST_CASE("NURBS", "[nurbs]") {
         auto p2 = curve.evaluate(1.0);
         REQUIRE(p2[0] == Approx(2.0));
         REQUIRE(p2[1] == Approx(0.0));
+    }
+
+    SECTION("Circles") {
+        SECTION("3-way split") {
+            constexpr float R = 1.1;
+            Eigen::Matrix<float, 1, 2> c(0.0, R);
+            Eigen::Matrix<float, 7, 2> control_pts;
+            control_pts << 0.0, 0.0,
+                           sqrt(3)*R, 0.0,
+                           0.5*sqrt(3)*R, 1.5*R,
+                           0.0, 3*R,
+                          -0.5*sqrt(3)*R, 1.5*R,
+                          -sqrt(3)*R, 0.0,
+                           0.0, 0.0;
+            Eigen::Matrix<float, 10, 1> knots;
+            knots << 0.0, 0.0, 0.0,
+                     1.0/3.0, 1.0/3.0,
+                     2.0/3.0, 2.0/3.0,
+                     1.0, 1.0, 1.0;
+
+            Eigen::Matrix<float, 7, 1> weights;
+            weights << 1.0, 0.5, 1.0, 0.5, 1.0, 0.5, 1.0;
+
+            NURBS<float, 2, 2, true> curve;
+            curve.set_control_points(control_pts);
+            curve.set_knots(knots);
+            curve.set_weights(weights);
+            curve.initialize();
+
+            REQUIRE(curve.get_degree() == 2);
+
+            for (float t=0.0; t<1.01; t+=0.2) {
+                const auto p = curve.evaluate(t);
+                REQUIRE((p-c).norm() == Approx(R));
+            }
+        }
+
+        SECTION("4-way split") {
+            constexpr float R = 12.1;
+            Eigen::Matrix<float, 1, 2> c(0.0, R);
+            Eigen::Matrix<float, 9, 2> control_pts;
+            control_pts << 0.0, 0.0,
+                           R, 0.0,
+                           R, R,
+                           R, 2*R,
+                           0.0, 2*R,
+                           -R, 2*R,
+                           -R, R,
+                           -R, 0.0,
+                           0.0, 0.0;
+            Eigen::Matrix<float, 12, 1> knots;
+            knots << 0.0, 0.0, 0.0,
+                     0.25, 0.25,
+                     0.5, 0.5,
+                     0.75, 0.75,
+                     1.0, 1.0, 1.0;
+            Eigen::Matrix<float, 9, 1> weights;
+            weights << 1.0, sqrt(2)/2,
+                       1.0, sqrt(2)/2,
+                       1.0, sqrt(2)/2,
+                       1.0, sqrt(2)/2,
+                       1.0;
+
+            NURBS<float, 2, 2, true> curve;
+            curve.set_control_points(control_pts);
+            curve.set_knots(knots);
+            curve.set_weights(weights);
+            curve.initialize();
+
+            REQUIRE(curve.get_degree() == 2);
+
+            for (float t=0.0; t<1.01; t+=0.2) {
+                const auto p = curve.evaluate(t);
+                REQUIRE((p-c).norm() == Approx(R));
+            }
+        }
     }
 }
--- tests/test_RationalBezier.cpp
+++ tests/test_RationalBezier.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include <cmath>
+
 #include <nanospline/RationalBezier.h>
 
 TEST_CASE("RationalBezier", "[rational][bezier]") {
@@ -108,6 +110,55 @@ TEST_CASE("RationalBezier", "[rational][bezier]") {
 
             REQUIRE((start-control_pts.row(0)).norm() == Approx(0.0));
             REQUIRE((end-control_pts.row(2)).norm() == Approx(0.0));
+        }
+    }
+
+    SECTION("Circular arc") {
+        // Rational quadratic Bezier is capable of representing circular arc.
+        // Let's check.
+
+        SECTION("Quarter circle") {
+            const float R = 1.2;
+            RationalBezier<float, 2, 2, true> curve;
+
+            Eigen::Matrix<float, 3, 2> control_pts;
+            control_pts << R, 0.0,
+                           R, R,
+                           0.0, R;
+            curve.set_control_points(control_pts);
+
+            Eigen::Matrix<float, 3, 1> weights;
+            weights << 1.0, sqrt(2) / 2, 1.0;
+            curve.set_weights(weights);
+            curve.initialize();
+
+            for (float t=0.0; t<1.01; t+=0.2) {
+                const auto p = curve.evaluate(t);
+                REQUIRE(p.norm() == Approx(R));
+            }
+        }
+
+        SECTION("One third circle") {
+            const float R = 2.5;
+            Eigen::Matrix<float, 1, 2> c(0.0, R);
+            RationalBezier<float, 2, 2, true> curve;
+
+            Eigen::Matrix<float, 3, 2> control_pts;
+            control_pts << 0.5 * sqrt(3) * R, 1.5*R,
+                           0.0, 3*R,
+                          -0.5 * sqrt(3) * R, 1.5*R;
+                            
+            curve.set_control_points(control_pts);
+
+            Eigen::Matrix<float, 3, 1> weights;
+            weights << 1.0, 0.5, 1.0;
+            curve.set_weights(weights);
+            curve.initialize();
+
+            for (float t=0.0; t<1.01; t+=0.2) {
+                const auto p = curve.evaluate(t);
+                REQUIRE((p-c).norm() == Approx(R));
+            }
         }
     }
 }
