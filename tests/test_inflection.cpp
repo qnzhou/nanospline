8350b4ba7027494ff3433c4aaabf4aec4ec3d3c9
Qingnan Zhou <qzhou@adobe.com>
2019-12-05 20:18:28 -0500

    Refactor unit tests.

--- tests/test_inflection.cpp
+++ tests/test_inflection.cpp
@@ -133,7 +133,7 @@ TEST_CASE("inflection", "[inflection]") {
         }
     }
 
-    SECTION("RationalBezier with uniform weights") {
+    SECTION("RationalBezier") {
         RationalBezier<Scalar, 2, 3, true> curve;
 
         Eigen::Matrix<Scalar, 4, 2> control_pts;
@@ -142,41 +142,26 @@ TEST_CASE("inflection", "[inflection]") {
                        1.0,-1.0,
                        1.0, 0.0;
         curve.set_control_points(control_pts);
-        REQUIRE(curve.get_degree() == 3);
-
-        Eigen::Matrix<Scalar, 4, 1> weights;
-        weights.setConstant(1.1);
-
-        curve.set_weights(weights);
-        curve.initialize();
-        auto inflections = compute_inflections(curve, 1e-12, 1.0-1e-12);
-
-        CHECK(inflections.size() == 1);
-        for (auto t : inflections) {
-            auto curvature = curve.evaluate_curvature(t);
-            REQUIRE(curvature.norm() == Approx(0.0).margin(1e-12));
-        }
-    }
-
-    SECTION("RationalBezier with non-uniform weights") {
-        RationalBezier<Scalar, 2, 3, true> curve;
-
-        Eigen::Matrix<Scalar, 4, 2> control_pts;
-        control_pts << 0.0, 0.0,
-                       0.0, 1.0,
-                       1.0,-1.0,
-                       1.0, 0.0;
-        curve.set_control_points(control_pts);
-        REQUIRE(curve.get_degree() == 3);
 
         Eigen::Matrix<Scalar, 4, 1> weights;
         int expected_num_inflections = 0;
 
-        weights << 1.0, 1.0, 0.0, 1.0;
-        expected_num_inflections = 0;
+        SECTION("Uniform weights") {
+            weights.setConstant(1.1);
+            expected_num_inflections = 1;
+        }
+        SECTION("Non-uniform weights") {
+            weights << 1.0, 1.0, 0.0, 1.0;
+            expected_num_inflections = 0;
+        }
+        SECTION("Large weights") {
+            weights << 1.0, 1.0, 20.0, 1.0;
+            expected_num_inflections = 1;
+        }
 
         curve.set_weights(weights);
         curve.initialize();
+        REQUIRE(curve.get_degree() == 3);
         auto inflections = compute_inflections(curve, 1e-12, 1.0-1e-12);
 
         CHECK(inflections.size() == expected_num_inflections);
