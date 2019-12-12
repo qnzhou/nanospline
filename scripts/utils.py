ddb7fdcf359caf2d834726808724749dabfaae1e
Teseo Schneider <teseo@bluewin.ch>
2019-12-12 13:35:54 -0500

    added optimal turning angle and refactored code gen

---
+++ scripts/utils.py
@@ -0,0 +1,80 @@
+from sympy import *
+from scipy.special import comb
+import numpy as np
+
+def bezier(degree, t, rational, syms):
+    tot = 0
+    n = degree
+
+    denom = 0
+    for i in range(n+1):
+        if rational:
+            tot += int(comb(n, i)) * (1-t)**(n-i) * \
+                t**i * syms[i][0] * syms[i][1]
+        else:
+            tot += int(comb(n, i)) * (1-t)**(n-i) * t**i * syms[i][0]
+        denom += int(comb(n, i)) * (1-t)**(n-i) * t**i * syms[i][1]
+
+    if rational:
+        return tot/denom
+
+    return tot
+
+
+def setup_functions(maxb=11, maxrb=5):
+    functions = {}
+
+    beziers = []
+
+    for i in range(2, maxb):
+        beziers.append((i+1, i, False, lambda t, syms,
+                        i=i: bezier(i, t, False, syms)))
+
+    functions["Bezier"] = beziers
+
+    rbeziers = []
+
+    for i in range(2, maxrb):
+        rbeziers.append((i+1, i, True, lambda t, syms,
+                         i=i: bezier(i, t, True, syms)))
+
+    functions["RationalBezier"] = rbeziers
+
+    return functions
+
+
+def create_coeff_symbols(n_coeffs):
+    syms = []
+
+    for i in range(n_coeffs):
+        syms.append((
+            np.array([
+                symbols('cx{}'.format(i)),
+                symbols('cy{}'.format(i))]),
+            symbols('w{}'.format(i))))
+
+    return syms
+
+
+def generate_solver_code(n_coeffs, coeffs, is_rational, poly, printer):
+    lines = []
+    lines.append("const auto& ctrl_pts = curve.get_control_points();")
+
+    if is_rational:
+        lines.append("const auto& weights = curve.get_weights();")
+
+    for i in range(n_coeffs):
+        lines.append("Scalar cx{0} = ctrl_pts({0}, 0);".format(i))
+        lines.append("Scalar cy{0} = ctrl_pts({0}, 1);".format(i))
+        if is_rational:
+            lines.append("Scalar w{0} = weights({0});".format(i))
+
+    lines.append(
+        "PolynomialRootFinder<Scalar, {}>::find_real_roots_in_interval({{".format(poly.degree()))
+
+    lines.append(",\n".join(
+        ["    {}".format(printer.doprint(c)) for c in coeffs]))
+    lines.append("},")
+    lines.append("result, t0, t1, tol);")
+
+    return lines
---
+++ tests/test_optimal_turing_angle.cpp
@@ -0,0 +1,50 @@
+#include <catch2/catch.hpp>
+#include <iostream>
+
+#include <nanospline/optimal_turning_angle.h>
+#include <nanospline/split.h>
+#include <nanospline/save_svg.h>
+
+TEST_CASE("opt_turn_angle", "[opt_turn_angle]") {
+    using namespace nanospline;
+    using Scalar = double;
+
+    SECTION("Simple") {
+        Eigen::Matrix<Scalar, 4, 2, Eigen::RowMajor> control_pts;
+        control_pts << 0.0, 0.0,
+                       0.0, 1.0,
+                       1.0, 1.0,
+                       1.0, 0.0;
+
+        using Curve = Bezier<Scalar, 2, 3>;
+        Curve curve;
+        curve.set_control_points(control_pts);
+        auto t = optimal_points_to_reduce_turning_angle(curve, false);
+
+        REQUIRE(t.size() == 1);
+        REQUIRE(t[0] == Approx(0.5));
+
+        t = OptimalPointsToReduceTurningAngle<Curve>::compute(curve, false);
+        REQUIRE(t.size() == 1);
+    }
+
+    SECTION("Simple")
+    {
+        Eigen::Matrix<Scalar, 4, 2, Eigen::RowMajor> control_pts;
+        control_pts << 0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            1.0, 0.0;
+
+        using Curve = Bezier<Scalar, 2, 3>;
+        Curve curve;
+        curve.set_control_points(control_pts);
+        auto t = optimal_points_to_reduce_turning_angle(curve, false, 0.1, 0.4);
+
+        REQUIRE(t.size() == 1);
+        REQUIRE(t[0] == Approx(.2670068914));
+
+        t = OptimalPointsToReduceTurningAngle<Curve>::compute(curve, false, 0.1, 0.4);
+        REQUIRE(t.size() == 1);
+    }
+}
