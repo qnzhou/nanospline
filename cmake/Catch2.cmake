5118b283deb3ec4235b3477113489962a7814915
Qingnan Zhou <qzhou@adobe.com>
2020-03-29 17:10:04 -0700

    Use fetch contents for dependencies.

---
+++ cmake/Catch2.cmake
@@ -0,0 +1,14 @@
+include_guard()
+
+FetchContent_Declare(
+    Catch2
+    GIT_REPOSITORY git@github.com:catchorg/Catch2.git
+    GIT_TAG        v2.11.3
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_GetProperties(Catch2)
+if(NOT Catch2_POPULATED)
+    FetchContent_Populate(Catch2)
+    add_subdirectory(${catch2_SOURCE_DIR} ${catch2_BINARY_DIR})
+endif()
---
+++ cmake/Eigen3.cmake
@@ -0,0 +1,20 @@
+include_guard()
+
+FIND_PACKAGE(Eigen3)
+IF (NOT TARGET Eigen3::Eigen)
+
+    FetchContent_Declare(
+        Eigen
+        GIT_REPOSITORY git@gitlab.com:libeigen/eigen.git
+        GIT_TAG        3.3.7
+        GIT_SHALLOW TRUE
+    )
+
+    FetchContent_GetProperties(Eigen)
+    if(NOT Eigen_POPULATED)
+        FetchContent_Populate(Eigen)
+        add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR})
+    endif()
+
+ENDIF()
+
