3f2ae498cf939104c2522dfc170d6a892bf13c65
Qingnan Zhou <qzhou@adobe.com>
2020-10-09 16:38:05 -0700

    Add support for arc length on patch.

--- include/nanospline/arc_length.h
+++ include/nanospline/arc_length.h
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <nanospline/CurveBase.h>
-#include <nanospline/Quadrature.h>
+#include <nanospline/PatchBase.h>
 
 namespace nanospline {
+
+namespace internal {
+
+/**
+ * This function implement Romberg's method to integrate speed function from t0
+ * to t1.
+ */
+template <typename Scalar>
+Scalar romberg(std::function<Scalar(Scalar)> speed, Scalar t0, Scalar t1, size_t level);
+
+} // namespace internal
 
 /**
  * Given a parameter value t, this method computes the arc length at t using Romberg's method.
@@ -11,43 +22,40 @@ namespace nanospline {
 template <typename Scalar, int DIM>
 Scalar arc_length(const CurveBase<Scalar, DIM>& curve, Scalar t, size_t level = 10)
 {
-    auto speed = [&](Scalar tt) {
+    std::function<Scalar(Scalar)> speed = [&](Scalar tt) -> Scalar {
         const auto d1 = curve.evaluate_derivative(tt);
         return d1.norm();
     };
 
-    constexpr Scalar tol = 1e-12;
-
     const Scalar t0 = curve.get_domain_lower_bound();
-    std::vector<Scalar> R_values(level + 1, 0.0);
-    Scalar h = t - t0;
-    R_values[0] = 0.5 * h * (speed(t0), +speed(t));
-    for (size_t i = 1; i <= level; i++) {
-        h /= 2;
-        Scalar R_above = R_values[0];
+    return internal::romberg(speed, t0, t, level);
+}
 
-        R_values[0] = 0.5 * R_above;
-        size_t num_steps = (size_t)1 << (i - 1);
-        for (size_t k = 1; k < num_steps; k++) {
-            R_values[0] += speed(t0 + static_cast<Scalar>(2 * k - 1) * h) * h;
-        }
+/**
+ * Compute arc length between two points on a patch.
+ */
+template <typename Scalar, int DIM>
+Scalar arc_length(const PatchBase<Scalar, DIM>& patch,
+    Scalar u0,
+    Scalar v0,
+    Scalar u1,
+    Scalar v1,
+    size_t level = 10)
+{
+    const Eigen::Matrix<Scalar, 1, 2> dir = {u1 - u0, v1 - v0};
 
-        size_t m = 1;
-        Scalar R_prev_above = R_above;
-        for (size_t j = 1; j <= i; j++) {
-            m *= 4;
-            R_above = R_values[j];
-            R_values[j] =
-                R_values[j - 1] + (R_values[j - 1] - R_prev_above) / static_cast<Scalar>(m - 1);
-            R_prev_above = R_above;
-        }
+    auto position = [&](Scalar t) {
+        return Eigen::Matrix<Scalar, 1, 2>((u1 - u0) * t + u0, (v1 - v0) * t + v0);
+    };
 
-        if (i > 1 && std::abs(R_values[i] - R_values[i - 1]) < tol) {
-            return R_values[i];
-        }
-    }
+    std::function<Scalar(Scalar)> speed = [&](Scalar t) -> Scalar {
+        const auto p = position(t);
+        const auto du = patch.evaluate_derivative_u(p[0], p[1]);
+        const auto dv = patch.evaluate_derivative_v(p[0], p[1]);
+        return (du * dir[0] + dv * dir[1]).norm();
+    };
 
-    return R_values[level];
+    return internal::romberg(speed, (Scalar)0, (Scalar)1, level);
 }
 
 /**
@@ -84,4 +92,81 @@ Scalar inverse_arc_length(const CurveBase<Scalar, DIM>& curve, Scalar l, int num
     return t;
 }
 
+template <typename Scalar, int DIM>
+Eigen::Matrix<Scalar, 1, 2> inverse_arc_length(const PatchBase<Scalar, DIM>& patch,
+    Scalar u0,
+    Scalar v0,
+    Scalar u1,
+    Scalar v1,
+    Scalar l,
+    int num_iterations = 10)
+{
+    const Eigen::Matrix<Scalar, 1, 2> dir = {u1 - u0, v1 - v0};
+
+    auto position = [&](Scalar t) {
+        return Eigen::Matrix<Scalar, 1, 2>((u1 - u0) * t + u0, (v1 - v0) * t + v0);
+    };
+
+    std::function<Scalar(Scalar)> speed = [&](Scalar t) -> Scalar {
+        const auto p = position(t);
+        const auto du = patch.evaluate_derivative_u(p[0], p[1]);
+        const auto dv = patch.evaluate_derivative_v(p[0], p[1]);
+        return (du * dir[0] + dv * dir[1]).norm();
+    };
+
+    const auto L = arc_length(patch, u0, v0, u1, v1);
+    Scalar t = l / L;
+
+    const Scalar tol = L * 1e-12;
+
+    for (int i = 0; i < num_iterations; i++) {
+        const auto p = position(t);
+        const Scalar err = arc_length(patch, u0, v0, p[0], p[1]) - l;
+        if (std::abs(err) < tol) {
+            break;
+        }
+        t = t - err / speed(t);
+    }
+
+    return position(t);
+}
+
+namespace internal {
+
+template <typename Scalar>
+Scalar romberg(std::function<Scalar(Scalar)> speed, Scalar t0, Scalar t1, size_t level)
+{
+    constexpr Scalar tol = 1e-12;
+    std::vector<Scalar> R_values(level + 1, 0.0);
+    Scalar h = t1 - t0;
+    R_values[0] = 0.5 * h * (speed(t0) + speed(t1));
+    for (size_t i = 1; i <= level; i++) {
+        h /= 2;
+        Scalar R_above = R_values[0];
+
+        R_values[0] = 0.5 * R_above;
+        size_t num_steps = (size_t)1 << (i - 1);
+        for (size_t k = 1; k < num_steps; k++) {
+            R_values[0] += speed(t0 + static_cast<Scalar>(2 * k - 1) * h) * h;
+        }
+
+        size_t m = 1;
+        Scalar R_prev_above = R_above;
+        for (size_t j = 1; j <= i; j++) {
+            m *= 4;
+            R_above = R_values[j];
+            R_values[j] =
+                R_values[j - 1] + (R_values[j - 1] - R_prev_above) / static_cast<Scalar>(m - 1);
+            R_prev_above = R_above;
+        }
+
+        if (i > 1 && std::abs(R_values[i] - R_values[i - 1]) < tol) {
+            return R_values[i];
+        }
+    }
+
+    return R_values[level];
+}
+
+} // namespace internal
 } // namespace nanospline
--- tests/test_arc_length.cpp
+++ tests/test_arc_length.cpp
@@ -2,11 +2,13 @@
 
 #include <vector>
 
+#include <nanospline/BSpline.h>
+#include <nanospline/Bezier.h>
+#include <nanospline/BezierPatch.h>
+#include <nanospline/NURBS.h>
 #include <nanospline/arc_length.h>
 #include <nanospline/forward_declaration.h>
-#include <nanospline/Bezier.h>
-#include <nanospline/BSpline.h>
-#include <nanospline/NURBS.h>
+#include <nanospline/save_obj.h>
 #include "validation_utils.h"
 
 TEST_CASE("arc_length", "[arc_length]")
@@ -28,6 +30,34 @@ TEST_CASE("arc_length", "[arc_length]")
             const Scalar t1 = (Scalar)i / (Scalar)N * (t_max - t_min) + t_min;
             const Scalar t2 = inverse_arc_length(curve, lengths[i]);
             REQUIRE(t1 == Approx(t2));
+        }
+    };
+
+    auto check_arc_length_on_patch = [](auto& patch) {
+        const auto u_min = patch.get_u_lower_bound();
+        const auto u_max = patch.get_u_upper_bound();
+        const auto v_min = patch.get_v_lower_bound();
+        const auto v_max = patch.get_v_upper_bound();
+
+        const auto diag_1 = arc_length(patch, u_min, v_min, u_max, v_max);
+        const auto diag_2 = arc_length(patch, u_max, v_max, u_min, v_min);
+        const auto diag_3 = arc_length(patch, u_min, v_max, u_max, v_min);
+        const auto diag_4 = arc_length(patch, u_max, v_min, u_min, v_max);
+
+        REQUIRE(diag_1 == Approx(diag_2));
+        REQUIRE(diag_3 == Approx(diag_4));
+
+        constexpr size_t N = 10;
+        for (size_t i = 0; i <= N; i++) {
+            Scalar t = (Scalar)i / (Scalar)N;
+
+            Scalar u = u_min + t * (u_max - u_min);
+            Scalar v = v_min + t * (v_max - v_min);
+
+            auto l = arc_length(patch, u_min, v_min, u, v);
+            auto p = inverse_arc_length(patch, u_min, v_min, u_max, v_max, l);
+            REQUIRE(p[0] == Approx(u).margin(1e-6 * (u_max - u_min)));
+            REQUIRE(p[1] == Approx(v).margin(1e-6 * (v_max - v_min)));
         }
     };
 
@@ -55,31 +85,17 @@ TEST_CASE("arc_length", "[arc_length]")
         check_arc_length(curve);
     }
 
-    SECTION("Quarter circle") {
+    SECTION("Quarter circle")
+    {
         constexpr Scalar R = 12.1;
         Eigen::Matrix<Scalar, 1, 2> c(0.0, R);
         Eigen::Matrix<Scalar, 9, 2> control_pts;
-        control_pts << 0.0, 0.0,
-                    R, 0.0,
-                    R, R,
-                    R, 2*R,
-                    0.0, 2*R,
-                    -R, 2*R,
-                    -R, R,
-                    -R, 0.0,
-                    0.0, 0.0;
+        control_pts << 0.0, 0.0, R, 0.0, R, R, R, 2 * R, 0.0, 2 * R, -R, 2 * R, -R, R, -R, 0.0, 0.0,
+            0.0;
         Eigen::Matrix<Scalar, 12, 1> knots;
-        knots << 0.0, 0.0, 0.0,
-              0.25, 0.25,
-              0.5, 0.5,
-              0.75, 0.75,
-              1.0, 1.0, 1.0;
+        knots << 0.0, 0.0, 0.0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1.0, 1.0, 1.0;
         Eigen::Matrix<Scalar, 9, 1> weights;
-        weights << 1.0, sqrt(2)/2,
-                1.0, sqrt(2)/2,
-                1.0, sqrt(2)/2,
-                1.0, sqrt(2)/2,
-                1.0;
+        weights << 1.0, sqrt(2) / 2, 1.0, sqrt(2) / 2, 1.0, sqrt(2) / 2, 1.0, sqrt(2) / 2, 1.0;
 
         NURBS<Scalar, 2, 2, true> curve;
         curve.set_control_points(control_pts);
@@ -88,5 +104,31 @@ TEST_CASE("arc_length", "[arc_length]")
         curve.initialize();
 
         check_arc_length(curve);
+    }
+
+    SECTION("Bilinear patch")
+    {
+        BezierPatch<Scalar, 3, 1, 1> patch;
+        Eigen::Matrix<Scalar, 4, 3> control_grid;
+        control_grid << 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0;
+        patch.set_control_grid(control_grid);
+        patch.initialize();
+
+        check_arc_length_on_patch(patch);
+    }
+
+    SECTION("Cubic patch")
+    {
+        BezierPatch<Scalar, 3, 3, 3> patch;
+        Eigen::Matrix<Scalar, 16, 3> control_grid;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                control_grid.row(i * 4 + j) << j, i, ((i + j) % 2 == 0) ? -1 : 1;
+            }
+        }
+        patch.set_control_grid(control_grid);
+        patch.initialize();
+
+        check_arc_length_on_patch(patch);
     }
 }
