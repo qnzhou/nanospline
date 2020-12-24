2c508779ddbae4f404efca275698c986e2eb38c3
Qingnan Zhou <qzhou@adobe.com>
2020-12-23 20:57:46 -0500

    Add a single include header.

---
+++ include/nanospline/nanospline.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// Curve types.
+#include <nanospline/BSpline.h>
+#include <nanospline/Bezier.h>
+#include <nanospline/NURBS.h>
+#include <nanospline/RationalBezier.h>
+
+// Patch types.
+#include <nanospline/BSplinePatch.h>
+#include <nanospline/BezierPatch.h>
+#include <nanospline/NURBSPatch.h>
+#include <nanospline/RationalBezierPatch.h>
+
+// IO
+#include <nanospline/load_msh.h>
+#include <nanospline/save_msh.h>
+#include <nanospline/save_obj.h>
+#include <nanospline/save_svg.h>
+
+// Utilities
+#include <nanospline/arc_length.h>
+#include <nanospline/conversion.h>
+#include <nanospline/hodograph.h>
+#include <nanospline/split.h>
